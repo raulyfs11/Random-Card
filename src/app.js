e13c567ad362b8ea74b96d0ba31223a40f4acd5f
raulyfs11 <rauljulio9823@gmail.com>
2023-03-14 04:49:38 +0000

    final

--- src/app.js
+++ src/app.js
@@ -1,27 +1,43 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-function GenerateNumber() {
-  let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+function getSuit() {
+  let suit = ["♦", "♥", "♠", "♣"];
+  let suIndex = Math.floor(Math.random() * suit.length);
+  return suit[suIndex];
+}
+
+function getNumber() {
+  let number = [
+    "8",
+    "3",
+    "7",
+    "9",
+    "6",
+    "4",
+    "A",
+    "5",
+    "K",
+    "Q",
+    "2",
+    "J",
+    "10"
+  ];
 
   let numIndex = Math.floor(Math.random() * number.length);
-  return numIndex;
+  return number[numIndex];
 }
-
-function GenerateSuits() {
-  let suits = ["♥", "♦", "♠", "♣"];
-
-  let suIndex = Math.floor(Math.random() * suits.length);
-  return suIndex;
-}
-
-window.onload = function() {
-  //write your code here
-  document.getElementById("number").innerHTML = GenerateNumber();
-  document.getElementsByClassName("suits").innerHTML = GenerateSuits();
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.getElementById("number").innerText = getNumber();
+  let s = getSuit();
+  document.getElementById("top-suit").innerText = s;
+  document.getElementById("bottom-suit").innerText = s;
+  if (s == "♥" || s == "♦") {
+    document.getElementById("top-suit").classList.add("red");
+    document.getElementById("bottom-suit").classList.add("red");
+    document.getElementById("number").classList.add("red");
+  }
 };
